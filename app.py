2a8b320af235b6f5ec2444ca577f3efcc98e889a
KylerPsenka <47467749+KylerPsenka@users.noreply.github.com>
2025-04-16 18:40:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -65,3 +65,8 @@ def get_stock():
         print("❌ Internal server error:")
         traceback.print_exc()
         return jsonify({'error': 'internal server error'}), 500
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
